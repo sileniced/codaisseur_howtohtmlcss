057e4b29a7454fd1542268431429949ccbc32af0
Vince Liem <vincepaul.liem@gmail.com>
2018-09-12 14:59:41 +0200

    moved the bars into a JS file for easy insertion

--- bars/js/add_top_bar.js
+++
@@ -1,6 +0,0 @@
-document.write('<ul class="top-bar">\n' +
-  '    <li><a href="index.html">Mimi\'s teachables</a></li>\n' +
-  '    <li><a href="content/html/index.html">HTML</a></li>\n' +
-  '    <li><a href="content/css/index.html">CSS</a></li>\n' +
-  '    <li><a href="content/clean_code/index.html">CLEAN CODE</a></li>\n' +
-  '</ul>\n');
